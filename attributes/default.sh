b3f7f95c0452a3024fec4562b1b3cbc90c10d781
jimmy yang <gankkank@gmail.com>
2014-05-07 16:03:29 +0800

    init project

---
+++ attributes/default.sh
@@ -0,0 +1,6 @@
+compile_src_version="0.10.28"
+compile_src_url="http://nodejs.org/dist/v${compile_src_version}/node-v${compile_src_version}.tar.gz"
+compile_src_path=/opt/srcv
+compile_src_prefix=/opt/node-${compile_src_version}
+compile_src_configure=""
+compile_src_install="make,make install"
---
+++ recipes/default.sh
@@ -0,0 +1,5 @@
+
+
+compile from_url "$compile_src_url"
+
+set_basic_path
